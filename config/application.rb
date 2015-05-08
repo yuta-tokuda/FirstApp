a53cf6f7ebe838a76a7f6adb360d8caa012c1549
yuta.tokuda <yuta.tokuda@luxiar.com>
2015-05-08 10:30:32 +0900

    Update config/application.rb for Heroku

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,9 @@ module FirstApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
   end
 end
+# for heroku deploy
+config.assets.initialize_on_precompile = false
+
